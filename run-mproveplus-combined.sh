b6c894d8f5bcd4e83062ff4de3a5eb4c9d962f8d
Piyush Bagad <piyush@wadhwaniai.org>
2020-08-22 16:33:30 +0530

    Latest output files

--- run-mproveplus-combined.sh
+++ run-mproveplus-combined.sh
@@ -26,21 +26,21 @@
 # Note: timings ESTIMATED based on Intel® Core™ i7-5500U CPU @ 2.40GHz (on a single core)
 
 cargo build --release
-#cargo run --release --bin mprove_plus_ristretto_bin 150 100 -n 1
-#cargo run --release --bin mprove_plus_ristretto_bin 250 100 -n 1
-#cargo run --release --bin mprove_plus_ristretto_bin 300 100 -n 1
-#cargo run --release --bin mprove_plus_ristretto_bin 400 100 -n 1
-#cargo run --release --bin mprove_plus_ristretto_bin 600 100 -n 1
-#cargo run --release --bin mprove_plus_ristretto_bin 1200 100 -n 1
-#cargo run --release --bin mprove_plus_ristretto_bin 2000 100 -n 1
-#cargo run --release --bin mprove_plus_ristretto_bin 2500 100 -n 1
-#cargo run --release --bin mprove_plus_ristretto_bin 4500 100 -n 1
-#cargo run --release --bin mprove_plus_ristretto_bin 5000 100 -n 1
-#cargo run --release --bin mprove_plus_ristretto_bin 8000 100 -n 1
-#cargo run --release --bin mprove_plus_ristretto_bin 10000 100 -n 1
-#cargo run --release --bin mprove_plus_ristretto_bin 16000 100 -n 1
-#cargo run --release --bin mprove_plus_ristretto_bin 30000 100 -n 1
-#cargo run --release --bin mprove_plus_ristretto_bin 35000 100 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 150 100 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 250 100 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 300 100 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 400 100 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 600 100 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 1200 100 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 2000 100 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 2500 100 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 4500 100 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 5000 100 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 8000 100 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 10000 100 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 16000 100 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 30000 100 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 35000 100 -n 1
 # cargo run --release --bin mprove_plus_ristretto_bin 60000 100 -n 1
 # cargo run --release --bin mprove_plus_ristretto_bin 80000 100 -n 1
 
@@ -62,21 +62,21 @@ cargo build --release
 # -----------------------------------------------------------------------------------------------
 # Total (hrs) =	6.5823			6.5156	0.0503
 
-#cargo run --release --bin mprove_plus_ristretto_bin 5000 50 -n 1
-#cargo run --release --bin mprove_plus_ristretto_bin 5000 50 -n 1
-#cargo run --release --bin mprove_plus_ristretto_bin 5000 90 -n 1
-#cargo run --release --bin mprove_plus_ristretto_bin 5000 100 -n 1
-#cargo run --release --bin mprove_plus_ristretto_bin 5000 200 -n 1
-#cargo run --release --bin mprove_plus_ristretto_bin 5000 500 -n 1
-#cargo run --release --bin mprove_plus_ristretto_bin 5000 800 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 5000 50 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 5000 50 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 5000 90 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 5000 100 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 5000 200 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 5000 500 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 5000 800 -n 1
 # cargo run --release --bin mprove_plus_ristretto_bin 5000 1200 -n 1
 # cargo run --release --bin mprove_plus_ristretto_bin 5000 1500 -n 1
 # cargo run --release --bin mprove_plus_ristretto_bin 5000 2000 -n 1
 # cargo run --release --bin mprove_plus_ristretto_bin 5000 2500 -n 1
 
 # Heavy memory usage simulations to be left later to run
-#cargo run --release --bin mprove_plus_ristretto_bin 60000 100 -n 1
-#cargo run --release --bin mprove_plus_ristretto_bin 80000 100 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 60000 100 -n 1
+cargo run --release --bin mprove_plus_ristretto_bin 80000 100 -n 1
 
 cargo run --release --bin mprove_plus_ristretto_bin 5000 1200 -n 1
 cargo run --release --bin mprove_plus_ristretto_bin 5000 1500 -n 1
